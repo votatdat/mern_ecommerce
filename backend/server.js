84f5c03b00e63343b8798cd9c5eb7b8c2fa8f259
Vo Tat Dat <tatdat13@gmail.com>
2021-04-01 11:36:08 +0700

    14. Env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
 
+dotenv.config()
 
 const app = express();
 
@@ -17,4 +19,7 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server is running on http://localhost:5000/'))
+const PORT = process.env.PORT || 5000
+const MODE = process.env.NODE_ENV || 'development'
+
+app.listen(5000, console.log(`Server is running in ${MODE} mode on http://localhost:${PORT}/`))
