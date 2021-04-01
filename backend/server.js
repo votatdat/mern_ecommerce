6a9a7826724ddd88d194f2be0b29639a6bc0f4e4
Vo Tat Dat <tatdat13@gmail.com>
2021-04-01 11:46:46 +0700

    15. ES modules, finished section 3

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 dotenv.config()
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running......')
