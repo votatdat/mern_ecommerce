f59a2c732c71d76cd830fe1ab51c172b11cae0c6
Vo Tat Dat <tatdat13@gmail.com>
2021-04-20 09:38:14 +0700

    36. Clean up productRoute by using controllers

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
+            <Nav className="ml-auto">
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i>Cart
