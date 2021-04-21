2e073e57131583d223ca5f18279f87b7ae1ac6f3
Vo Tat Dat <tatdat13@gmail.com>
2021-04-21 22:22:41 +0700

    fixed some errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from "./screens/CartScreen"
-import LoginScreen from './screens/UserScreen'
+import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 
 
@@ -28,4 +28,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default App
