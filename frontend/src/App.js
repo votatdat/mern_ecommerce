7535e5a3b5873fdc4ea2dd864b8955adb4217873
Vo Tat Dat <tatdat13@gmail.com>
2021-04-22 11:05:47 +0700

    53. Payment screen & Save payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 
 const App = () => {
@@ -23,13 +24,14 @@ const App = () => {
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
     </Router>
-  );
+  )
 }
 
 export default App
