d9a9ed65a5db8943e1050e7e7862e88124ab1e79
Vo Tat Dat <tatdat13@gmail.com>
2021-04-22 17:34:36 +0700

    58. Order details reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,10 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import {orderCreateReducer} from "./reducers/orderReducers"
+import {
+  orderCreateReducer,
+  orderDetailReducer,
+} from "./reducers/orderReducers"
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
