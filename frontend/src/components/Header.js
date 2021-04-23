04b03aef025dc2b4da040444b73d853298ddf2bc
Vo Tat Dat <tatdat13@gmail.com>
2021-04-23 16:32:55 +0700

    67. Admin user list

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -29,25 +29,37 @@ const Header = () => {
                   <i className="fas fa-shopping-cart" />Cart
                 </Nav.Link>
               </LinkContainer>
-              {userInfo
-                ? (
-                  <NavDropdown id='username' title={userInfo.name}>
-                    <LinkContainer to='/profile'>
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                )
-                : (
-                  <LinkContainer to='/login'>
-                    <Nav.Link>
-                      <i className="fas fa-user" />Sign In
-                    </Nav.Link>
+              {userInfo ?
+              (
+                <NavDropdown id='username' title={userInfo.name}>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
-                )}
-
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) :
+              (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className="fas fa-user" />Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown id='adminmenu' title='Admin'>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/productlist'>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              ) }
             </Nav>
           </Navbar.Collapse>
         </Container>
