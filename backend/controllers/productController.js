1af2797fbe721662898844bc968c23e924c1b39c
Vo Tat Dat <tatdat13@gmail.com>
2021-04-24 23:54:45 +0700

    87. Top products carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ import Product from '../models/productModel.js'
 // @route    GET api/products
 // @access   Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 3
+  const pageSize = 4
   const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword ? {
@@ -145,6 +145,18 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc     Get top rated products
+// @route    GET /api/products/top
+// @access   Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product
+    .find({})
+    .sort({rating: -1})
+    .limit(3)
+
+  res.json(products)
+})
+
 export {
   getProducts,
   getProductById,
@@ -152,4 +164,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js'
 import {protect, admin} from '../middleware/authMiddleware.js'
 
@@ -16,6 +17,7 @@ router
   .get(getProducts)
   .post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router
   .route('/:id')
   .get(getProductById)
